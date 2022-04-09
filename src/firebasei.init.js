7515d38d9132b24473310af2aefd6d4a51cdb0cc
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-04-09 14:30:22 +0600

    Header and login and home added

---
+++ src/firebasei.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ1dZJVQ5Y0pjwT8wCyjVu2E9lIAGhxY8",
+  authDomain: "router-firebase-app-5b133.firebaseapp.com",
+  projectId: "router-firebase-app-5b133",
+  storageBucket: "router-firebase-app-5b133.appspot.com",
+  messagingSenderId: "476602127053",
+  appId: "1:476602127053:web:ee6884050db4735cbccf84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
